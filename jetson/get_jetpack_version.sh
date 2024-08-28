c38ff6f808beea3bd04c1eed0315c1b4ab650fba
Ian M <ianf.mcmurray@gmail.com>
2024-08-28 12:36:53 -0700

    feat: jetson scripts - get JP and Kernel

---
+++ jetson/get_jetpack_version.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+uname -r
+# e.g.
+# 5.10.120-tegra
---
+++ jetson/get_kernel_version.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cat /etc/nv_tegra_release
+# e.g.
+# # R35 (release), REVISION: 4.1, GCID: 33958178, BOARD: t186ref, EABI: aarch64, DATE: Tue Aug  1 19:57:35 UTC 2023
