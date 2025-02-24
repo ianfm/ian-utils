00cc12041569fe5fb95780d5b7221c2714bf38f3
Ian McMurray <ianf.mcmurray@gmail.com>
2025-02-24 09:32:33 -0800

    reminder on how to push local code to a fresh repo on github

---
+++ git_push_local_repo.sh
@@ -0,0 +1,6 @@
+# From the new repo starter page on github
+# prereqs: git init .; git add .; git commit -m "first commit"
+#          created EMPTY repo on github
+git remote add origin git@github.com:ianfm/pelagic-boat-os.git
+git branch -M main
+git push -u origin main
